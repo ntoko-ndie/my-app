2c0e2dfb7d48cb9d01462f3ae66fcdf7a53a72f5
Ntoko Ndie <ntoko.ndie@gmail.com>
2022-03-04 10:50:25 -0600

    created navigation.css and modified navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,8 +1,35 @@
+import './navigation.css';
 function Navigation() {
     return (
-        <div>
-            This will be the navigation
-        </div>
+        <header id="header" className="d-flex flex-column justify-content-center">
+            <nav id="navbar" className="navbar nav-menu">
+                <ul>
+                    <li>
+                        <a href="#hero" className="nav-link scrollto active"
+                        ><i className="bx bx-home"/><span>Home</span></a
+                        >
+                    </li>
+                    <li>
+                        <a href="#about" className="nav-link scrollto"
+                        ><i className="bx bx-user"/><span>About</span></a
+                        >
+                    </li>
+
+                    <li>
+                        <a href="#projects" className="nav-link scrollto"
+                        ><i className="bx bx-book-content"/> <span>Projects</span></a
+                        >
+                    </li>
+
+                    <li>
+                        <a href="#contact" className="nav-link scrollto"
+                        ><i className="bx bx-envelope"/> <span>Contact</span></a
+                        >
+                    </li>
+                </ul>
+            </nav>
+
+        </header>
     );
 }
 
